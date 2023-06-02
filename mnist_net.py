473c6d40471f34a4ed21ac27b12ccc1e82f1877b
Alexandre Personnic <alexandre.personnic@capgemini.com>
2023-06-02 09:55:59 +0200

    def network

--- mnist_net.py
+++ mnist_net.py
@@ -2,25 +2,24 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-import torch
-import torch.nn as nn
-import torch.nn.functional as F
-
 class MNISTNet(nn.Module):
     def __init__(self):
         super(MNISTNet, self).__init__()
-        self.conv1 = nn.Conv2d(...)
-        self.conv2 = nn.Conv2d(...)
-        self.pool = nn.MaxPool2d(...)
-        self.fc1 = nn.Linear(...)
-        self.fc2 = nn.Linear(...)
-        self.fc3 = nn.Linear(...)
+        self.conv1 = nn.Conv2d(1, 8, 5)
+        self.conv2 = nn.Conv2d(8, 16, 5)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.fc1 = nn.Linear(256, 128)
+        self.fc2 = nn.Linear(128, 64)
+        self.fc3 = nn.Linear(64, 10)
+        self.flatten = nn.Flatten()
 
     def forward(self, x):
         x = F.relu(self.conv1(x))       # First convolution followed by
         x = self.pool(x)                # a relu activation and a max pooling#
-        x = ...
-        ...
+        x = F.relu(self.conv2(x))
+        x = self.pool(x)
+        x = self.fc1(self.flatten(x))
+        x = self.fc2(x)
         x = self.fc3(x)
         return x
 
@@ -31,5 +30,6 @@ class MNISTNet(nn.Module):
 if __name__=='__main__':
     x = torch.rand(16,1,28,28)
     net = MNISTNet()
+    print(net)
     y = net(x)
     assert y.shape == (16,10)
